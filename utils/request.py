e5235e351b684cfeb7f658b6071bb0fa2116acf4
Isaacdevs <imthimun@student.wethinkcode.co.za>
2022-05-22 13:15:44 +0200

    updated the request url into the latest AWS ip address

--- utils/request.py
+++ utils/request.py
@@ -36,8 +36,9 @@ feature_vector_json = test.iloc[1].to_json()
 # NOTE: When testing your instance of the API on a remote machine
 # replace the URL below with its public IP:
 
-url = 'http://ec2-52-49-182-98.eu-west-1.compute.amazonaws.com:5000/api_v0.1'
+
 # url = 'http://127.0.0.1:5000/api_v0.1'
+url = 'http://54.170.180:5000/api_v0.1'
 
 # Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
